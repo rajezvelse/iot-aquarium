9d55e97ef1feeda2f529dde98b68d6e099fda4e9
Rajez <rajezvelse@gmail.com>
2023-12-07 22:10:05 +0530

    Cron execution skip if execution time is greater than buffer time

--- lib/cron.py
+++ lib/cron.py
@@ -7,15 +7,22 @@ schedules = [
     (aiot.CO2_OFF_TIME, aiot.switch_off_co2, 23.59),
     (aiot.LIGHT_ON_TIME, aiot.switch_on_light, aiot.LIGHT_OFF_TIME),
     (aiot.LIGHT_OFF_TIME, aiot.switch_off_light, 23.59),
-    (aiot.FEEDING_TIMES[0], aiot.start_food_feed, aiot.FEEDING_TIMES[0] + aiot.FEEDING_BUFFER),
-    (aiot.FEEDING_TIMES[1], aiot.start_food_feed, aiot.FEEDING_TIMES[1] + aiot.FEEDING_BUFFER),
+    (
+        aiot.FEEDING_TIMES[0],
+        aiot.start_food_feed,
+        aiot.FEEDING_TIMES[0] + aiot.FEEDING_BUFFER,
+    ),
+    (
+        aiot.FEEDING_TIMES[1],
+        aiot.start_food_feed,
+        aiot.FEEDING_TIMES[1] + aiot.FEEDING_BUFFER,
+    ),
 ]
 
 if __name__ == "__main__":
-    
     aiot.log("Starting cron")
     aiot.setup()
-    
+
     try:
         for schedule in schedules:
             t, fn, buffer = schedule
@@ -25,27 +32,33 @@ if __name__ == "__main__":
             exec_time = aiot.get_dt(t)
             buff_time = aiot.get_dt(buffer)
 
+            eligible = exec_time < buff_time
             in_next_x_mins = now <= exec_time < now + timedelta(minutes=30)
             in_past = exec_time < now
 
             last_execution_time = aiot.get_previous_state(fn_name)
-            already_executed = last_execution_time and exec_time <= last_execution_time < buff_time
-            
-            if (in_past and already_executed) or (not in_past and not in_next_x_mins):
+            already_executed = (
+                last_execution_time and exec_time <= last_execution_time < buff_time
+            )
+
+            if (
+                not eligible
+                or (in_past and already_executed)
+                or (not in_past and not in_next_x_mins)
+            ):
                 continue
-            
-            aiot.log("Executing " + fn_name)    
+
+            aiot.log("Executing " + fn_name)
             fn()
             aiot.set_previous_state(fn_name, now)
 
-        
         aiot.log("Finished cron")
 
     except Exception as e:
         aiot.log("Error: " + str(e), "error")
-        
+
         # aiot.clean()
 
         # os.system("sudo reboot")
-    #finally:
-        # aiot.clean()
+    # finally:
+    # aiot.clean()
