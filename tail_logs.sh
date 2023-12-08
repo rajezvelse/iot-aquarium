c24caeefb66bbdc9e090d85b0dda34b3564cd8c0
Rajez <rajezvelse@gmail.com>
2023-12-08 22:13:51 +0530

    tail_logs update

--- tail_logs.sh
+++ tail_logs.sh
@@ -4,4 +4,4 @@ export APP_DIR=/home/pi/iot/
 export PYTHONPATH=$APP_DIR
 cd "$APP_DIR/logs"
 
-tail  -n 50 -f /var/log/cron.log "$(ls -t | head -n 1)" 
+tail  -n 50 -f /var/log/cron.log "$(ls -t aquarium_iot* | head -n 1)"  "$(ls -t sync_log* | head -n 1)"
