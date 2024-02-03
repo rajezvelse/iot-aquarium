1c1eb01123ba30d2bf301a0060e4f5fa7b5ef26e
Rajez <rajezvelse@gmail.com>
2024-02-03 18:49:06 +0530

    Increase servo time to 3s

--- lib/aquarium_iot.py
+++ lib/aquarium_iot.py
@@ -118,7 +118,7 @@ def servo_360(pin):
     p.start(0)  # Starts running PWM on the pin and sets it to 0
 
     p.ChangeDutyCycle(7.4)
-    sleep(2.4)
+    sleep(3)
     p.stop()
     sleep(200 / 1000)  # 100 millisecods
     GPIO.setup(pin, GPIO.IN)
